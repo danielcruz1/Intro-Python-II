a15a501068d4ed39ed4ef9c9bede862e920610ed
Daniel  Cruz <danielcruz489@gmail.com>
2020-07-14 19:33:32 -0600

    added player class and room direction

--- src/room.py
+++ src/room.py
@@ -4,7 +4,14 @@
 class Room:
     def __init__(self, name, description): 
         self.name = name
-        self.direnction = description
+        self.description = description
+        self.n
+        self.s
+        self.e
+        self.w
 
-room = Room([], [])
+    def __str__ (self):
+        return f'{self.name} has {len(self.description)} description'
+ 
+room = Room([], [""])
 print(room)
